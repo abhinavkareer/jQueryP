84035047556097d28c2cae209f166f1cc8306682
unknown <abhinavkareer@gmail.com>
2015-12-30 16:17:24 -0800

    added slider to my practice Jquery

---
+++ includes/js/slider.js
@@ -0,0 +1,35 @@
+(function($){
+  var sliderUl=$('div.slider').css("overflow","hidden").children('ul'),
+  imgs=sliderUl.find('img'),
+  imgWidth=imgs[0].width,//225
+  imgsLen=imgs.length,//4
+  current=1,
+  totalImgsWidth=imgWidth*imgsLen;
+  $('#slider-nav').show().find('button').on('click',function(){
+    var direction=$(this).data('dir'),
+    loc=imgWidth;
+    // update current value
+    (direction=='next') ? ++current:--current;
+    if(current==0)
+    {
+      current=imgsLen;
+      loc=totalImgsWidth-imgWidth;
+      direction="next";
+    }
+    else if((current-1)==imgsLen)
+    {
+      current=1;
+      loc=0;
+    }
+    transition(sliderUl,loc,direction);
+  });
+  function transition(container,loc,direction){
+    var unit; // += | -=
+    if(direction && loc !==0)
+    {
+      unit=  (direction==='next') ? "-=" : "+=";
+    }
+    container.animate({'margin-left': unit ? (unit+loc) : loc})
+  }
+
+})(jQuery);
